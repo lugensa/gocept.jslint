acbca0678d3bc7dd812c42a22d28bb35ea2f60fa
Wolfgang Schnerring <ws@gocept.com>
2011-08-08 10:32:50 +0200

    Create egg skeleton

---
+++ README.txt
@@ -0,0 +1,8 @@
+=============
+gocept.jslint
+=============
+
+This package integrates Douglas Crockford's JSLint tool (https://jslint.com)
+with Python's unittest module.
+
+XXX writeme
---
+++ setup.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2011 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='gocept.jslint',
+    version='0.1dev',
+    author='Wolfgang Schnerring',
+    author_email='ws@gocept.com',
+    url='http://pypi.python.org/pypi/gocept.jslint',
+    description="""\
+Python-unittest integration for jslint.
+""",
+    long_description=(
+        open('README.txt').read()
+        + '\n\n'
+        + open('CHANGES.txt').read()),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    license='ZPL',
+    namespace_packages=['gocept'],
+    install_requires=[
+        'setuptools',
+    ],
+    extras_require=dict(test=[
+    ]),
+)
