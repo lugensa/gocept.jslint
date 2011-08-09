8c5b7b6f0b17815e4e69ffdc00afc4121c66c8b3
Wolfgang Schnerring <wosc@wosc.de>
2011-08-09 14:59:31 +0200

    Implement simple variant of collecting files and running jslint on them

--- src/gocept/jslint/__init__.py
+++ src/gocept/jslint/__init__.py
@@ -1,1 +1,4 @@
-# python package
+# Copyright (c) 2011 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+from gocept.jslint.case import TestCase
---
+++ src/gocept/jslint/case.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2011 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+import glob
+import os.path
+import pkg_resources
+import subprocess
+import unittest
+
+
+class JSLintTestGenerator(type):
+
+    def __new__(cls, name, bases, dict):
+        for filename in cls._collect_files(dict['include']):
+            cls._create_jslint_runner(dict, filename)
+        return type.__new__(cls, name, bases, dict)
+
+    @classmethod
+    def _create_jslint_runner(cls, dict, filename):
+        name = 'test_jslint_%s' % os.path.basename(filename)
+        dict[name] = lambda x: x._run_jslint(filename)
+
+    @classmethod
+    def _collect_files(cls, include):
+        paths = []
+        for path in include:
+            package, path = path.split(':', 1)
+            paths.append(pkg_resources.resource_filename(package, path))
+
+        files = []
+        for path in paths:
+            files.extend(glob.glob(os.path.join(path, '*.js')))
+        return files
+
+
+class TestCase(unittest.TestCase):
+
+    __metaclass__ = JSLintTestGenerator
+
+    include = []
+    options = ['--sloppy', '--browser', '--nomen',
+               '--white', '--vars', '--sub']
+
+    def _run_jslint(self, filename):
+        jslint = pkg_resources.resource_filename(
+            'gocept.jslint.js', 'node-jslint.js')
+        subprocess.call(['node', jslint] + self.options + [filename])
